ac8a13a4594452c7ec11c8d1f47c90f1b167abb7
zhenbinjing <729494296@qq.com>
2016-04-19 17:35:51 +0800

    0419

--- gulpfile.js
+++ gulpfile.js
@@ -16,22 +16,22 @@ var processhtml = require('gulp-processhtml');				//- html更改模板
 var htmlmin = require('gulp-htmlmin');					//- html压缩
 var browserSync = require('browser-sync');				//- 同步测试工具
 
-var y_Dz="src";								//- 生产环境路径
-var y_Sz="dist";							//- 上线环境路径
+var y_Sz="src";								//- 生产环境路径
+var y_Dz="dist";							//- 上线环境路径
 
 gulp.task('imagemin',function(){
-	gulp.src('./'+y_Dz+'/img/*.{png,jpg,gif,ico}')
+	gulp.src('./'+y_Sz+'/img/*.{png,jpg,gif,ico}')
 	.pipe(cache(imagemin({
 	progressive: true,
 	svgoPlugins: [{removeViewBox: false}],				//- 不要移除svg的viewbox属性
 	use: [pngquant()]						//- 使用pngquant深度压缩png图片的imagemin插件							
 	})))
-	.pipe(gulp.dest('./'+y_Sz+'/img'));				//- 输出路径
+	.pipe(gulp.dest('./'+y_Dz+'/img'));				//- 输出路径
 });
 
 gulp.task('concat',function(){						//- 创建一个名为 concat 的 task
 	var date=new Date().getTime();					//- 创建版本时间	
-	gulp.src(['./'+y_Dz+'/css/*.css'])				//- 需要处理的css文件，放到一个字符串数组里	
+	gulp.src(['./'+y_Sz+'/css/*.css'])				//- 需要处理的css文件，放到一个字符串数组里	
 	.pipe(replace(/_VERSION_/gi,date))				//- 文件指纹							
 	.pipe(concat('index.css'))					//- 合并后的文件名
 	.pipe(autoprefixer({
@@ -45,48 +45,48 @@ gulp.task('concat',function(){						//- 创建一个名为 concat 的 task
 	remove:true							//- 是否去掉不必要的前缀 默认：true 
 	}))
 	.pipe(uncss({
-	html: ['./'+y_Dz+'/*.html'],					//- 检查的页面
+	html: ['./'+y_Sz+'/*.html'],					//- 检查的页面
 	ignore: ['abc','.abc','#abc']					//- 忽略的标签 class or id or 分号隔开
 	}))
 	.pipe(minifyCss({compatibility: 'ie8', keepBreaks:false}))	//- 压缩处理成一行,兼容ie
-	.pipe(gulp.dest('./'+y_Sz+'/css'));				//- 输出文件本地
+	.pipe(gulp.dest('./'+y_Dz+'/css'));				//- 输出文件本地
 });
 
 gulp.task('jsmin',function(){						//- 合并多个文件
-	gulp.src(['./'+y_Dz+'/js/*.js'])				//- 多个文件以数组形式传入
+	gulp.src(['./'+y_Sz+'/js/*.js'])				//- 多个文件以数组形式传入
 	.pipe(uglify())
 	.pipe(concat('index.js'))  
-	.pipe(gulp.dest('./'+y_Sz+'/js'));
+	.pipe(gulp.dest('./'+y_Dz+'/js'));
 });
 
 gulp.task('jshint',function(){						//- 检查文件
-	gulp.src('./'+y_Sz+'/js/*.js')
+	gulp.src('./'+y_Dz+'/js/*.js')
 	.pipe(jshint())
 	.pipe(jshint.reporter('default'));				//- 检查错误
 });
 
 gulp.task('processhtml',function(){					//- 修改该html的dom
 	var date = new Date().getTime();
-	gulp.src('./'+y_Dz+'/*.html')
+	gulp.src('./'+y_Sz+'/*.html')
 	.pipe(replace(/_VERSION_/gi, date))
 	.pipe(processhtml())
-	.pipe(gulp.dest('./'+y_Sz+'/'));
+	.pipe(gulp.dest('./'+y_Dz+'/'));
 });
 
 gulp.task('fs',function(){
-	return FontSpider(['./'+y_Dz+'/*.html']);		//- 删除多余的字体和图标，添加return返回最终的数据流		
+	return FontSpider(['./'+y_Sz+'/*.html']);		//- 删除多余的字体和图标，添加return返回最终的数据流		
 });		
 
 gulp.task('cp',['fs'],function(){					//- 先把fs命令执行完后，再去执行cp命令，fs需要添加return
-	gulp.src(['./'+y_Dz+'/font/**'],{				//- 被复制的文件夹下的所有文件
-	base: './'+y_Dz+'/font'						//- 被复制的目标路径  
+	gulp.src(['./'+y_Sz+'/font/**'],{				//- 被复制的文件夹下的所有文件
+	base: './'+y_Sz+'/font'						//- 被复制的目标路径  
 	})
-	.pipe(gulp.dest('./'+y_Sz+'/font'))				//- 输出路径	
+	.pipe(gulp.dest('./'+y_Dz+'/font'))				//- 输出路径	
 	
-	gulp.src(['./'+y_Dz+'/icon/**'],{				//- 被复制的文件夹下的所有文件
-	base: './'+y_Dz+'/icon'						//- 被复制的目标路径  
+	gulp.src(['./'+y_Sz+'/icon/**'],{				//- 被复制的文件夹下的所有文件
+	base: './'+y_Sz+'/icon'						//- 被复制的目标路径  
 	})
-	.pipe(gulp.dest('./'+y_Sz+'/icon'));
+	.pipe(gulp.dest('./'+y_Dz+'/icon'));
 });
 
 /*-------------bs,html这两个命令是需要时手动执行-----------------*/
@@ -98,15 +98,15 @@ gulp.task('html',function(){
 	minifyJS: true,							//- 压缩页面JS
 	minifyCSS: true							//- 压缩页面CSS
 };
-gulp.src('./'+y_Sz+'/*.html')						//- 压缩页面路径
+gulp.src('./'+y_Dz+'/*.html')						//- 压缩页面路径
 	.pipe(htmlmin(options))
-	.pipe(gulp.dest('./'+y_Sz+'/'));				//- 输出路径	
+	.pipe(gulp.dest('./'+y_Dz+'/'));				//- 输出路径	
 });
 
 gulp.task('bs',function(){
 	browserSync({
 	files: "**",							//- 监控所有文件
-	server: {baseDir: './'+y_Dz+'/'}				//- 目标文件夹路径
+	server: {baseDir: './'+y_Sz+'/'}				//- 目标文件夹路径
 	});
 });
 
