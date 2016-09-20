05994ca0bb55063b46c58438dc5003001a71b2c4
zhenbinjing <729494296@qq.com>
2016-09-20 16:29:34 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 ﻿//npm install gulp --save-dev
-//npm install gulp-concat gulp-imagemin imagemin-pngquant gulp-webp gulp-cache gulp-autoprefixer gulp-minify-css gulp-px3rem gulp-uncss gulp-sass gulp-font-spider gulp-uglify gulp-jshint gulp-replace gulp-processhtml gulp-htmlmin browser-sync del --save-dev
+//npm install gulp-concat gulp-imagemin imagemin-pngquant gulp-webp gulp-cache gulp-autoprefixer gulp-clean-css gulp-px3rem gulp-uncss gulp-sass gulp-font-spider gulp-uglify gulp-jshint gulp-replace gulp-processhtml gulp-htmlmin browser-sync del --save-dev
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');					//- 多个文件合并为一个
@@ -8,7 +8,7 @@ var pngquant = require('imagemin-pngquant');				//- 深度压缩png插件
 var webp = require('gulp-webp');					//- 转webp图片
 var cache = require('gulp-cache');					//- 只压缩修改的图片，没有修改的图片直接从缓存文件读取
 var autoprefixer = require('gulp-autoprefixer');			//- 补充浏览器前缀
-var minifyCss = require('gulp-minify-css');				//- 压缩CSS为一行
+var cleanCSS = require('gulp-clean-css');				//- 压缩CSS为一行
 var px3rem = require('gulp-px3rem');					//- px转rem
 var uncss = require('gulp-uncss');					//- 删除没用到的css
 var sass = require('gulp-sass');					//- scss文件编译
@@ -24,16 +24,7 @@ var del = require('del');						//- 管理文件
 var y_Sz="src";								//- 生产环境路径
 var y_Dz="dist";							//- 上线环境路径
 
-gulp.task('imagemin',function(){
-	gulp.src('./'+y_Sz+'/img/**/*.{png,jpg,gif,ico}')
-	.pipe(cache(imagemin({
-	progressive: true,
-	svgoPlugins: [{removeViewBox: false}],				//- 不要移除svg的viewbox属性
-	use: [pngquant()]})))						//- 使用pngquant深度压缩png图片的imagemin插件						
-	.pipe(gulp.dest('./'+y_Dz+'/img'));				//- 输出路径
-});
-
-gulp.task('concat',function(){						//- 创建一个名为 concat 的 task
+gulp.task('cssall',function(){						//- 创建一个名为 concat 的 task
 	var date=new Date().getTime();					//- 创建版本时间	
 	gulp.src(['./'+y_Sz+'/css/**/*.css'])				//- 需要处理的css文件，放到一个字符串数组里	
 	.pipe(replace(/_VERSION_/gi,date))				//- 文件指纹							
@@ -52,9 +43,18 @@ gulp.task('concat',function(){						//- 创建一个名为 concat 的 task
 	ignore: ['abc','.abc','#abc']					//- 忽略的标签 class or id or 分号隔开
 	}))
 	.pipe(px3rem({remUnit: 100}))					//- px/100转rem值，如果有不想转换的类在值后面加/*no*/
-	.pipe(minifyCss({compatibility: 'ie8', keepBreaks:false}))	//- 压缩处理成一行,兼容ie
+	.pipe(cleanCSS({compatibility: 'ie8', keepBreaks:false, keepSpecialComments: '*'}))	//- 压缩处理成一行,兼容ie
 	.pipe(concat('index.css'))					//- 合并后的文件名
 	.pipe(gulp.dest('./'+y_Dz+'/css'));				//- 输出文件本地
+});
+
+gulp.task('imagemin',function(){
+	gulp.src('./'+y_Sz+'/img/**/*.{png,jpg,gif,ico}')
+	.pipe(cache(imagemin({
+	progressive: true,
+	svgoPlugins: [{removeViewBox: false}],				//- 不要移除svg的viewbox属性
+	use: [pngquant()]})))						//- 使用pngquant深度压缩png图片的imagemin插件						
+	.pipe(gulp.dest('./'+y_Dz+'/img'));				//- 输出路径
 });
 
 gulp.task('jsmin',function(){						//- 合并多个文件
@@ -78,12 +78,12 @@ gulp.task('processhtml',function(){					//- 修改html的dom
 	.pipe(gulp.dest('./'+y_Dz+'/'));
 });
 
-gulp.task('fs',function(){
+gulp.task('fontSpider',function(){
 	return gulp.src(['./'+y_Sz+'/**/*.html'])			//- 删除多余的字体和图标，添加return返回最终的数据流
 	.pipe(fontSpider());
 });		
 
-gulp.task('cp',['fs'],function(){					//- 先把fs命令执行完后，再去执行cp命令，fs需要添加return
+gulp.task('copy',['fontSpider'],function(){				//- 先把fs命令执行完后，再去执行cp命令，fs需要添加return
 	gulp.src(['./'+y_Sz+'/font/**'],{				//- 被复制的文件夹下的所有文件
 	base: './'+y_Sz+'/font'})					//- 被复制的目标路径 	
 	.pipe(gulp.dest('./'+y_Dz+'/font'))				//- 输出路径	
@@ -129,7 +129,7 @@ gulp.task('webp_html',function(){
 	.pipe(gulp.dest('./'+y_Dz+'/'));
 });
  
-gulp.task('html',function(){										
+gulp.task('minhtml',function(){										
 	var options = {
 	removeComments: true,						//- 清除HTML注释
 	collapseWhitespace: true,					//- 压缩HTML
@@ -149,4 +149,4 @@ gulp.task('bs',function(){
 });
 
 //执行插件函数
-gulp.task('default',['concat','imagemin','jsmin','jshint','processhtml','cp','sass']);
+gulp.task('default',['cssall','imagemin','jsmin','jshint','processhtml','copy','sass']);
