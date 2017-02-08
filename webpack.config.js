2a3318dfee95c80dc5e6e56b99c0b7645faefca5
zhenbinjing <729494296@qq.com>
2017-02-08 18:02:43 +0800

    add base64

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@
 var webpack = require("webpack");
 module.exports = {
     entry:[ //接入
-    "./src/js/scrollLoad.js",
+	"./src/js/scrollLoad.js",
 	"./src/js/ts.js" 
 	],
     output:{//产出
         path:path.join(__dirname, "./dist/js"),
         filename:"index.js"
-    },
+	},
     plugins:[ //插件区
-    new webpack.optimize.UglifyJsPlugin({//压缩js  
+	new webpack.optimize.UglifyJsPlugin({//压缩js  
         compressor:{warnings:false}
-    }) 
+	}) 
 	]
 };
