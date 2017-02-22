a43004ed761dea8c7cdb1818ec1f8a69ce08ef26
zhenbinjing <729494296@qq.com>
2017-02-22 15:11:15 +0800

    add index

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,12 @@
 ﻿var TouchSlide = require('./plugins/TouchSlide.js');
 var scrollLoad = require('./plugins/scrollLoad.js');
-
 TouchSlide({ 
-	  slideCell:"#focus",
-	  titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
-	  mainCell:".bd ul", 
-	  effect:"left", 
-	  autoPlay:true,//自动播放
-	  autoPage:true,//自动分页
-	  switchLoad:"_src"
+	slideCell:"#focus",
+	titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
+	mainCell:".bd ul", 
+	effect:"left", 
+	autoPlay:true,//自动播放
+	autoPage:true,//自动分页
+	switchLoad:"_src"
 });
-
 scrollLoad();
