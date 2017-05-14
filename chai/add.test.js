1af31d49156a455a5e38744a8b984be674d5cc6c
zhenbinjing <729494296@qq.com>
2017-05-14 10:11:21 +0800

    add mocha chai istanbul

---
+++ chai/add.test.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+var add = require('../test/add.js');
+var expect = require('chai').expect;
+
+describe('加法函数的测试', function() {
+  it('1 加 1 应该等于 2', function() {
+    expect(add(1, 1)).to.be.equal(2);
+  });
+  it('1 加 1 不应该等于 3', function() {
+    expect(add(1, 1)).to.be.not.equal(3);
+  });
+});
