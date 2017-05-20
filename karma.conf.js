ee485d69a029f38e5789b00f47f94721b4e9513e
zhenbinjing <729494296@qq.com>
2017-05-20 16:47:07 +0800

    add tab

--- karma.conf.js
+++ karma.conf.js
@@ -2,48 +2,48 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = function(config) {
-  config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+	config.set({
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha','chai'],
+	// base path that will be used to resolve all patterns (eg. files, exclude)
+	basePath: '',
 
-    // list of files / patterns to load in the browser
-    files: [
+	// frameworks to use
+	// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+	frameworks: ['mocha','chai'],
+
+	// list of files / patterns to load in the browser
+	files: [
 		'./src/js/chai/index.js'
-    ],
+	],
 
-    // list of files to exclude
-    exclude: [],
+	// list of files to exclude
+	exclude: [],
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
+	// preprocess matching files before serving them to the browser
+	// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+	preprocessors: {
 		'./src/js/chai/index.js': ['webpack']
-    },
+	},
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec','coverage','coverage-istanbul'],
+	// test results reporter to use
+	// possible values: 'dots', 'progress'
+	// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+	reporters: ['spec','coverage','coverage-istanbul'],
 	  
 	webpack: {
         module: {
-            rules: [
+            rules: [{
                 // instrument only testing sources with Istanbul
-                {
+                
                     test: /\.js$/,
                     include: path.resolve('./src/js/chai'),
                     loader: 'istanbul-instrumenter-loader'
-                }
-            ]
-        }
-    },
+                }]   
+		}
+	},
 
-	 coverageIstanbulReporter: {       
+	coverageIstanbulReporter: {       
 		  reports: ['html', 'lcovonly', 'text-summary'],
 		  dir: './coverage', 
 		  'report-config': {
@@ -58,31 +58,32 @@ module.exports = function(config) {
         		}
 		  },
 		  fixWebpackSourcePaths: true		
-    },
-    // web server port
-    port: 9876,
+	},
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+	// web server port
+	port: 9876,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+	// enable / disable colors in the output (reporters and logs)
+	colors: true,
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+	// level of logging
+	// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+	logLevel: config.LOG_INFO,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+	// enable / disable watching file and executing tests whenever any file changes
+	autoWatch: true,
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+	// start these browsers
+	// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+	browsers: ['PhantomJS'],
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
+	// Continuous Integration mode
+	// if true, Karma captures browsers, runs the tests and exits
+	singleRun: true,
+
+	// Concurrency level
+	// how many browser should be started simultaneous
+	concurrency: Infinity
 	
   })
 }
--- karma.sauce.js
+++ karma.sauce.js
@@ -29,54 +29,55 @@ var customLaunchers = {
 var maxExecuteTime = 5*60*1000;
 
 module.exports = function(config) {
-if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+
+	if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
 		process.env.SAUCE_USERNAME = sauce.username;
 		process.env.SAUCE_ACCESS_KEY = sauce.accesskey;
-}
-  config.set({
+	}
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+	config.set({
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha','chai'],
+    	// base path that will be used to resolve all patterns (eg. files, exclude)
+    	basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
+	// frameworks to use
+	// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+	frameworks: ['mocha','chai'],
+
+
+	// list of files / patterns to load in the browser
+	files: [
 		'./src/js/chai/index.js'
-    ],
+	],
 
-    // list of files to exclude
-    exclude: [],
+	// list of files to exclude
+	exclude: [],
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
+	// preprocess matching files before serving them to the browser
+	// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+	preprocessors: {
 		'./src/js/chai/index.js': ['webpack']
-    },
+	},
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec','coverage','coverage-istanbul','saucelabs'],
+	// test results reporter to use
+	// possible values: 'dots', 'progress'
+	// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+	reporters: ['spec','coverage','coverage-istanbul','saucelabs'],
 	  
 	webpack: {
         module: {
-            rules: [
+            rules: [{
                 // instrument only testing sources with Istanbul
-                {
+                
                     test: /\.js$/,
                     include: path.resolve('./src/js/chai'),
                     loader: 'istanbul-instrumenter-loader'
-                }
-            ]
-        }
-    },
+		}]
+		}
+	},
 
-	 coverageIstanbulReporter: {       
+	coverageIstanbulReporter: {       
 		  reports: ['html', 'lcovonly', 'text-summary'],
 		  dir: './coverage', 
 		  'report-config': {
@@ -91,23 +92,25 @@ if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
         		}
 		  },
 		  fixWebpackSourcePaths: true		
-    },
-    // web server port
-    port: 9876,
+	},
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+	// web server port
+	port: 9876,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+	// enable / disable colors in the output (reporters and logs)
+	colors: true,
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+	// level of logging
+	// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+	logLevel: config.LOG_INFO,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    //browsers: ['PhantomJS'],
+	// enable / disable watching file and executing tests whenever any file changes
+	autoWatch: true,
+
+	// start these browsers
+	// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+	//browsers: ['PhantomJS'],
+
 	sauceLabs: {
 		public: 'public',
 		recordVideo: false,
@@ -115,19 +118,20 @@ if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
 		testName: 'icut unit tests',
 		build: 'build-' + Date.now()
 	},
+
 	customLaunchers: customLaunchers,
 	browsers: Object.keys(customLaunchers),
 	captureTimeout: maxExecuteTime,
 	browserNoActivityTimeout: maxExecuteTime,
 	retryLimit: 10, //为了保证都能运行这么多浏览器，必须添加重起的次数
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+	// Continuous Integration mode
+	// if true, Karma captures browsers, runs the tests and exits
+	singleRun: true,
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
+	// Concurrency level
+	// how many browser should be started simultaneous
+	concurrency: Infinity
 	
-  })
+	})
 }
