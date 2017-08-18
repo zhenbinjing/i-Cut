9a15cb14e6a3d40ce99ba5c3c42054113e2786d0
zhenbinjing <729494296@qq.com>
2017-08-18 16:39:42 +0800

    add babili to babel-minify

--- build/rollup.config.ts.js
+++ build/rollup.config.ts.js
@@ -1,15 +1,15 @@
-﻿import babili from 'rollup-plugin-babili';
+﻿import minify from 'rollup-plugin-babel-minify';
 //import resolve from 'rollup-plugin-node-resolve';
 //import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: './src/js/es6/main.js',
   dest: './src/js/es6/ts/main.js',	
-  format: 'es6',
+  format: 'es',
 //moduleName: 'icut', 
 //sourceMap: 'true',
   plugins: [ 
-	babili(),
+	minify()
 	//resolve({ jsnext: true, main: true, browser: true, }), 
 	//commonjs()
   ],
