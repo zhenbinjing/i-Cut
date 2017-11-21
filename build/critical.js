3f741d5c9e44037b08a0977bef1333fc4a105658
zhenbinjing <729494296@qq.com>
2017-11-21 19:02:53 +0800

    add critical

--- build/critical.js
+++ build/critical.js
@@ -1,16 +1,22 @@
-var path = require("path");	
-var critical = require('critical');
-var y_Dz = "dist";
-var lj = path.resolve(process.cwd(), y_Dz);
+const glob     = require('glob');
+const critical = require('critical');
 
-critical.generate({
-	inline: true,
-	base: lj,
-	src: 'index.html',     
-	width: 320,
-	height: 568,
-	minify: true,
-	ignore: ['@font-face',/url\(/],
-	//pathPrefix: '/dist/',
-	dest: 'index.html'
-})	
+function generateCriticalPath(err, files) {
+	if (err) throw err;
+	files.forEach((file, index) => {
+		const page = file.split('./').pop();
+			critical.generate({
+				inline: true,
+				base: './',
+				src: page,     
+				width: 320,
+				height: 568,
+				minify: true,
+				ignore: ['@font-face',/url\(/],
+				//pathPrefix: '/dist/',
+				dest: page
+			})	
+	});
+}
+
+glob(`./dist/*.html`, generateCriticalPath);
