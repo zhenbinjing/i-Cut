e8019cc743a5c3fcc7698655c5813684fdec588c
zhenbinjing <729494296@qq.com>
2018-01-10 13:50:51 +0800

    add test

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -80,7 +80,7 @@ module.exports = function(config) {
 
 	// start these browsers
 	// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-	browsers: ['MyHeadlessChrome'],
+	browsers: ['Chrome', 'ChromeHeadless', 'MyHeadlessChrome'],
 	customLaunchers: {
            MyHeadlessChrome: {
            base: 'ChromeHeadless',
