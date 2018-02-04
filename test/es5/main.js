8e0dc3f5ea2f525dbe2a12e874e0fe5ba84384a4
zhenbinjing <729494296@qq.com>
2018-02-04 19:15:59 +0800

    bf rollup

--- test/es5/main.js
+++ test/es5/main.js
@@ -1,1 +1,1 @@
-!function(){"use strict";var TSA=function(){};TSA.prototype.render=function(){return"AAAA"};(new TSA).render()}();
+!function(){"use strict";var TSA=function(){};TSA.prototype.render=function(){return"AAAA"},(new TSA).render()}();
