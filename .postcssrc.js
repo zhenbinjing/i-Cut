799ad393bba9b8b4afdd903ea81426b8f4b357c2
zhenbinjing <729494296@qq.com>
2018-03-24 15:23:19 +0800

    bf config

--- .postcssrc.js
+++ .postcssrc.js
@@ -4,6 +4,14 @@ module.exports = {
     "postcss-import": {},
     "postcss-px2rem": {remUnit: 100},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {
+      browsers: [
+        'last 2 version',                                 //- 主流浏览器的最新两个版本
+        'ios 7',                                          //- IOS7版本
+        'android 2.3',                                    //- android 2.3版本
+        'last 2 Explorer versions'],                      //- IE的最新两个版本 'last 2 Explorer versions'
+      cascade: true,                                    //- 是否美化属性值 默认：true 
+      remove:true                                       //- 是否去掉不必要的前缀 默认：true 
+    }
   }
 }
