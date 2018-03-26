324a6fb081871e66d529064590a19e7ae2e59324
zhenbinjing <729494296@qq.com>
2018-03-26 11:41:47 +0800

    bf css js

--- src/static/js/plugins/lazysizes.js
+++ src/static/js/plugins/lazysizes.js
@@ -1,5 +1,5 @@
 export default function lazysizes (){
-	(function(window, factory) {
+(function(window, factory) {
 	var lazySizes = factory(window, window.document);
 	window.lazySizes = lazySizes;
 	if(typeof module == 'object' && module.exports){
@@ -164,14 +164,14 @@ export default function lazysizes (){
 	var throttle = function(fn){
 		var running;
 		var lastTime = 0;
-		var gDelay = 125;
+		var gDelay = lazySizesConfig.throttleDelay;
 		var rICTimeout = lazySizesConfig.ricTimeout;
 		var run = function(){
 			running = false;
 			lastTime = Date.now();
 			fn();
 		};
-		var idleCallback = requestIdleCallback && lazySizesConfig.ricTimeout ?
+		var idleCallback = requestIdleCallback && rICTimeout > 49 ?
 			function(){
 				requestIdleCallback(run, {timeout: rICTimeout});
 
@@ -203,7 +203,7 @@ export default function lazysizes (){
 				delay = 0;
 			}
 
-			if(isPriority || (delay < 9 && requestIdleCallback)){
+			if(isPriority || delay < 9){
 				idleCallback();
 			} else {
 				setTimeout(idleCallback, delay);
@@ -260,7 +260,8 @@ export default function lazysizes (){
 			hFac: 0.8,
 			loadMode: 2,
 			loadHidden: true,
-			ricTimeout: 300,
+			ricTimeout: 0,
+			throttleDelay: 125,
 		};
 
 		lazySizesConfig = window.lazySizesConfig || window.lazysizesConfig || {};
