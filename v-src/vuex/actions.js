20a88ad0711532b6e0aff8534b968c28b804ca8b
zhenbinjing <729494296@qq.com>
2018-06-10 18:03:21 +0800

    bf axios

--- v-src/vuex/actions.js
+++ v-src/vuex/actions.js
@@ -4,7 +4,7 @@ import request from 'axios';
 
 export const getTopics = ({ commit, state }) =>
   request
-  .get(`/axios.json`)
+  .get(`http://i-cut.cc/axios.json`)
     .then(response => {
       if (response.statusText === 'OK') {
         commit('TOPICS_LIST', response.data);
