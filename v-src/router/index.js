1b9b3c1fa084333cdf7296419a4fc441ae9158ec
zhenbinjing <729494296@qq.com>
2018-07-13 13:17:06 +0800

    bf router

--- v-src/router/index.js
+++ v-src/router/index.js
@@ -2,15 +2,15 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 // 同步加载路由(服务端渲染)
-import vr1 from '../components/vr1.vue';
-// import axios from '../components/axios.vue';
-// import vuexs from '../components/vuexs.vue';
+import c_vr1 from '../components/vr1.vue';
+// import c_axios from '../components/axios.vue';
+// import c_vuexs from '../components/vuexs.vue';
 
 Vue.use(VueRouter);
 
 // 异步加载路由(客户端渲染)
-const axios = () => import('../components/axios.vue');
-const vuexs = () => import('../components/vuexs.vue');
+const c_axios = () => import('../components/axios.vue');
+const c_vuexs = () => import('../components/vuexs.vue');
 
 export default new VueRouter({
   // 去掉下面注释开启服务端渲染 history 模式
@@ -22,15 +22,15 @@ export default new VueRouter({
     },
     {
       path: '/vr1',
-      component: vr1
+      component: c_vr1
     },
     {
       path: '/axios',
-      component: axios
+      component: c_axios
     },
     {
       path: '/vuex',
-      component: vuexs
+      component: c_vuexs
     }
   ],
   linkActiveClass: 'active'
